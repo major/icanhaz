407413364970f6073a858ffa5cd0099b992b6553
carlgeorge <carl.george@rackspace.com>
2015-10-06 08:08:20 -0500

    undo absolute imports

--- icanhaz.py
+++ icanhaz.py
@@ -22,74 +22,74 @@ import socket
 import subprocess
 import time
 
-import flask
+from flask import Flask, request, send_from_directory
 
 
-app = flask.Flask(__name__, static_folder='static')
+app = Flask(__name__, static_folder='static')
 traceroute_bin = "/bin/traceroute-suid"
 
 
 @app.route("/")
 def icanhazafunction():
-    if 'icanhazptr' in flask.request.host:
+    if 'icanhazptr' in request.host:
         # The request is for *.icanhazptr.com
         try:
-            output = socket.gethostbyaddr(flask.request.remote_addr)
+            output = socket.gethostbyaddr(request.remote_addr)
             result = output[0]
         except:
-            result = flask.request.remote_addr
-    elif 'icanhazepoch' in flask.request.host:
+            result = request.remote_addr
+    elif 'icanhazepoch' in request.host:
         epoch_time = int(time.time())
         result = epoch_time
-    elif 'icanhaztrace' in flask.request.host:
+    elif 'icanhaztrace' in request.host:
         # The request is for *.icanhaztraceroute.com
         valid_ip = False
         try:
-            socket.inet_pton(socket.AF_INET, flask.request.remote_addr)
+            socket.inet_pton(socket.AF_INET, request.remote_addr)
             valid_ip = True
         except socket.error:
             pass
         try:
-            socket.inet_pton(socket.AF_INET6, flask.request.remote_addr)
+            socket.inet_pton(socket.AF_INET6, request.remote_addr)
             valid_ip = True
         except socket.error:
             pass
         if valid_ip:
-            if 'icanhaztraceroute' in flask.request.host:
+            if 'icanhaztraceroute' in request.host:
                 tracecmd = shlex.split("%s -q 1 -f 2 -w 1 %s" %
-                    (traceroute_bin, flask.request.remote_addr))
+                    (traceroute_bin, request.remote_addr))
             else:
                 tracecmd = shlex.split("%s -q 1 -f 2 -w 1 -n %s" %
-                    (traceroute_bin, flask.request.remote_addr))
+                    (traceroute_bin, request.remote_addr))
             result = subprocess.Popen(tracecmd,
                 stdout=subprocess.PIPE).communicate()[0].strip()
         else:
-            result = flask.request.remote_addr
-    elif 'icanhazproxy' in flask.request.host:
+            result = request.remote_addr
+    elif 'icanhazproxy' in request.host:
         proxy_headers = ['via', 'x-forwarded-for', 'forwarded', 'client-ip',
             'useragent_via', 'proxy_connection', 'xproxy_connection',
             'http_pc_remote_addr', 'http_client_ip',
             'http_x_appengine_country']
         found_headers = {}
         for header in proxy_headers:
-            value = flask.request.headers.get(header, None)
+            value = request.headers.get(header, None)
             if value:
                 found_headers[header] = value.strip()
         if len(found_headers) > 0:
             result = json.dumps(found_headers)
         else:
-            return flask.Response(""), 204
+            return Response(""), 204
     else:
         # The request is for *.icanhazip.com or something we don't recognize
-        result = flask.request.remote_addr
-    return flask.Response("%s\n" % result, mimetype="text/plain")
+        result = request.remote_addr
+    return Response("%s\n" % result, mimetype="text/plain")
 
 
 @app.route('/crossdomain.xml')
 @app.route('/humans.txt')
 @app.route('/robots.txt')
 def static_from_root():
-    return flask.send_from_directory(app.static_folder, flask.request.path[1:])
+    return send_from_directory(app.static_folder, request.path[1:])
 
 
 if __name__ == "__main__":
